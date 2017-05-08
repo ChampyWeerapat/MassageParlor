0c7136b77f0634fcbe800c18ae27a3e1744b4b30
Champy <weerapat.ka@ku.th>
2017-05-08 22:47:50 +0700

    modify massagist seeder

--- database/seeds/MassagistSeeder.php
+++ database/seeds/MassagistSeeder.php
@@ -14,18 +14,23 @@ class MassagistSeeder extends Seeder
         //
         DB::table('massagist')->insert([
             'name' => "Champ",
+            'imgpath' => "",
         ]);
         DB::table('massagist')->insert([
             'name' => "Meng",
+            'imgpath' => "",
         ]);
         DB::table('massagist')->insert([
             'name' => "Mick",
+            'imgpath' => "",
         ]);
         DB::table('massagist')->insert([
             'name' => "Air",
+            'imgpath' => "",
         ]);
         DB::table('massagist')->insert([
             'name' => "Namphueng",
+            'imgpath' => "",
         ]);
 
     }
