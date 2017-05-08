4d953576259503d1b9f202237639619eedeac402
Champy <weerapat.ka@ku.th>
2017-05-09 03:09:30 +0700

    query data from db in reserve page

--- resources/views/reservation.blade.php
+++ resources/views/reservation.blade.php
@@ -11,70 +11,179 @@
     </head>
     <body>
       @include('layouts._menubar')
+      <div style="padding:2%; padding-left:10%; padding-top:2%;">
+        <h1>Reserve Course</h1>
+      </div>
 
-
-
-        <div class="block-reserve">
+      <div class="row">
+        <div class="col col-md-8" style="padding-left:10%;">
+          <div class="block-reserve">
             <div class="container" id="vue-app">
-                <h1>Reserve Course</h1>
-		<form action="">
+              <form action="">
+                <div class="reserved-date">
+                  <div class="form-group">
+                    <div class="row">
+                      <div class="col col-md-2">
+                        <label>Date<label>
+                      </div>
+                      <div class="col col-md-1">
+                        <label>:</label>
+                      </div>
+                      <div class="col-6 col-md-4">
+                        <input type="date" id="date" class="form-control">
+                      </div>
+                    </div>
+                  </div>
+                </div>
 
-			<div class="reserved-date">
-				Date :
-				<input type="date" id="date">
-			</div>
+                <div class="course">
+                    <div class="row">
+                      <div class="col col-md-2">
+                        <label>Course</label>
+                      </div>
+                      <div class="col col-md-1">
+                        <label>:</label>
+                      </div>
+                      <div class="col-6 col-md-4">
+                        <div class="form-group">
+                          <select name="course-list" class="form-control">
+							@foreach ($courses as $course)
+                            <option value="$course->id" >
+                                     {{$course->description}}
+                            </option>
+                            @endforeach
+                          </select>
+                        </div>
+                      </div>
+                    </div>
+                </div>
 
-			<div class="reserved-massagist">
-				Would you like to specify the massager? :
-				<input type="radio"
-					   name="massagist"
-					   value="have"
-					   v-on:click="hasMassagist=!hasMassagist">
-					Yes
-				<input type="radio"
-					   name="massagist"
-					   value="dont-have"
-					   v-on:click="hasMassagist=!hasMassagist"
-					   checked>
-					No
-				<div v-if=hasMassagist>
-					<select name="massagist-list">
-						@foreach ($massagists as $massagist)
-							<option value="{{$massagist->id}}">
-								{{$massagist->name}}
-							</option>
-						@endforeach
-					</select>
-				</div>
-			</div>
+                <div class="reserved-massagist">
+                    <div class="row">
+                      <div class="col col-md-4">
+                        <label>Would you like to specify the massager?</label>
+                      </div>
+                      <div class="col col-md-1">
+                        <input type="radio" name="massagist" value="have" v-on:click="hasMassagist=!hasMassagist"> &nbsp; Yes
+                      </div>
+                      <div class="col col-md-1">
+                        <input type="radio" name="massagist" value="dont-have" v-on:click="hasMassagist=!hasMassagist" checked> &nbsp; No
+                      </div>
+                    </div>
+                </div>
 
-			<div class="reserved-table">
-				<!-- time table -->
-			</div>
+                <br />
 
-			<div class="show-redeem-point">
-				<p>
-					Your points : @{{ profile.point }}
-					(1 point = 1 baht)
-				</p>
-			</div>
+                <div v-if=hasMassagist>
+                  <div class="form-group">
+                      <div class="row">
+                        <div class="col col-md-1">
+                          <!-- Space  -->
+                        </div>
+                        <div class="col col-md-1">
+                          <label>Name<label>
+                        </div>
+                        <div class="col col-md-1">
+                          <label>:</label>
+                        </div>
+                        <div class="col-6 col-md-4">
+                          <select name="massagist-list" class="form-control">
+                            @foreach ($massagists as $massagist)
+                            <option value="$massagists->id" >
+                                     {{$massagist->name}}
+                            </option>
+                            @endforeach
+                          </select>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
 
-			<div class="voucher-field">
-				Redeem Voucher :
-				<input type="text" id="voucher-code">
-			</div>
+                <div class="table" v-if="hasMassagist">
+                  <div class="row">
+                    <div class="col col-md-6">
+                      <table class="table table-bordered">
+                        <thead>
+                          <tr>
+                            <th><center>Start</center></th>
+                            <th><center>Finish</center></th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <td></td>
+                            <td></td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                </div>
 
-			<div class="submit-button">
-				<button class="btn btn-default" v-on:click="submitReserve()">
-					Reserve
-				</button>
-			</div>
-		</form>
-	</div>
+                <div>
+                  <div class="form-group">
+                      <div class="row">
+                        <div class="col col-md-2">
+                          <label>Reserve time<label>
+                        </div>
+                        <div class="col col-md-1">
+                          <label>:</label>
+                        </div>
+                        <div class="col-6 col-md-4">
+                          <input type="time" id="time" class="form-control" >
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <br />
+                <div class="show-redeem-point" v-if="isMember">
+                  <p>
+                    Your points : @{{ profile.point }}
+                    (1 point = 1 Baht)
+                  </p>
+                </div>
+
+                <div class="show-redeem-point" v-if="!isMember">
+                  <p >
+                    You are not a member!! &nbsp;&nbsp;
+                    Register to be a member
+                    &nbsp;&nbsp; >> <a href="http://massageparlor.dev/register" style="color:brown">Click here softly</a>
+                  </p>
+                </div>
+
+                <div class="voucher-field">
+                  <div class="form-group">
+                    <div class="row">
+                      <div class="col col-md-2">
+                        <label>Redeem Voucher</label>
+                      </div>
+                      <div class="col col-md-1">
+                        <label>:</label>
+                      </div>
+                      <div class="col-6 col-md-4">
+                        <input type="text" id="voucher-code" class="form-control">
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+
+                <br>
+                <div class="submit-button">
+                  <button type="button" class="btn" v-on:click="submitReserve()">RESERVE</button>
+                </div>
+              </form>
+             </div>
+           </div>
         </div>
+
+
+        <div class="col col-md-4">
+          <div style="border:1px solid gray; border-radius:5px; margin-right:10%; height:450px; padding:20px;">
+              test
+          </div>
         </div>
-
-
+      </div>
 
         @include('layouts._footer')
 
@@ -90,10 +199,6 @@
         		id: '',
 	            name: '',
 	            point: 0
-        	},
-        	massagists: [],
-        	formMassagist: {
-        		name: ''
         	},
         	isMember: false
         },
